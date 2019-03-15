8d3da2584b3ea691da8b3b92b3f190296fc90eda
Nhu-Hoai Robert VO <nhuhoai.vo@franicflow.ch>
2019-03-15 16:17:39 +0100

    Create php on the fly check.

---
+++ src/index.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Main call file
+ * With test.php, print AB
+ *
+ * PHP Version 5.6, 7.0, 7.1, 7.2
+ *
+ * @category  Test
+ * @package   TravisApachePHP
+ * @author    Nhu-Hoai Robert VO <nhuhoai.vo@franicflow.ch>
+ * @copyright 2019 Nhu-Hoai Robert VO
+ * @license   http://www.opensource.org/licenses/mit-license.html MIT License
+ * @version   GIT: 0.1.0
+ * @link      https://www.nhuvo.ch/
+ * @since     0.1.0
+ */
+
+require_once __DIR__ . "/test.php";
+
+print "B";
---
+++ src/test.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Test file
+ * Print A
+ *
+ * PHP Version 5.6, 7.0, 7.1, 7.2
+ *
+ * @category  Test
+ * @package   TravisApachePHP
+ * @author    Nhu-Hoai Robert VO <nhuhoai.vo@franicflow.ch>
+ * @copyright 2019 Nhu-Hoai Robert VO
+ * @license   http://www.opensource.org/licenses/mit-license.html MIT License
+ * @version   GIT: 0.1.0
+ * @link      https://www.nhuvo.ch/
+ * @since     0.1.0
+ */
+
+print "A";
